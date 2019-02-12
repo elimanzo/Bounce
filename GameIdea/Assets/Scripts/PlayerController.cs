d46cd367dd4b82f91c1e2bfda0d515509cb70ee7
Aria <maria.zuk@icloud.com>
2019-02-11 17:59:07 -0800

    Boarder walls added, with tag “Fixed”, more collectable object behind WallBreak, changed colours and background, +passable door with hinges, raycast doesn't work

---
+++ GameIdea/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+void Update()
+{
+         RaycastHit hit;
+        Ray landingRay = new Ray(transform.position, Vector3.forward);
+        if(Physics.Raycast(landingRay, out hit, 1f))
+        {
+            if(hit.collider.tag=="Fixed")
+            {
+                rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
+            }
+        }}
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+   
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "WallBreak") {
+            Destroy(col.gameObject);
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
